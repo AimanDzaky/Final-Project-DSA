e61426173bdf11f52d774a4588291b7702ff1ca2
AvellinoVincent <147046450+AvellinoVincent@users.noreply.github.com>
2023-11-30 18:59:04 +0700

    TaxCalculator.cpp

--- TaxCalculator.cpp
+++ TaxCalculator.cpp
@@ -1,103 +1,106 @@
 #include <iostream>
-#include <cstring>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
-
-class DeductionNode {
-public:
-    char deductionName[50];
-    double deductionAmount;
-    DeductionNode* next;
-
-    DeductionNode(const char* name, double amount) : deductionAmount(amount), next(nullptr) {
-        strncpy(deductionName, name, sizeof(deductionName) - 1);
-    }
-};
-
-class DeductionList {
-public:
-    DeductionNode* head;
-
-    DeductionList() : head(nullptr) {}
-
-    void addDeduction(const char* name, double amount) {
-        DeductionNode* newNode = new DeductionNode(name, amount);
-        newNode->next = head;
-        head = newNode;
-    }
-
-    double calculateTotalDeductions() const {
-        double total = 0.0;
-        DeductionNode* current = head;
-        while (current != nullptr) {
-            total += current->deductionAmount;
-            current = current->next;
-        }
-        return total;
-    }
-
-    double calculateTotalDeductionsRecursive(DeductionNode* current) const {
-        if (current == nullptr) {
-            return 0.0;
-        }
-        return current->deductionAmount + calculateTotalDeductionsRecursive(current->next);
-    }
-};
 
 class TaxCalculator {
 private:
-    double income;
-    DeductionList deductions;
+    double userIncome;
+    double totalDeductions;
+    vector<pair<string, double>> deductions;
 
 public:
-    TaxCalculator(double initialIncome) : income(initialIncome) {}
+    TaxCalculator(double income, double deductions)
+        : userIncome(income), totalDeductions(deductions) {}
 
-    void addDeduction(const char* name, double amount) {
-        deductions.addDeduction(name, amount);
+    void addDeduction(string deductionName, double deductionAmount) {
+    totalDeductions += deductionAmount;
+    deductions.push_back(make_pair(deductionName, deductionAmount));
+}
+
+    void sortDeductions() {
+        sort(deductions.begin(), deductions.end());
     }
 
-    double calculateTax() const {
-        double totalDeductions = deductions.calculateTotalDeductions();
-        return 0.15 * (income - totalDeductions);
+    void printDeductions() const {
+        for (const auto& deduction : deductions) {
+            cout << setw(20) << left << deduction.first + ":" << setw(10) << right << "$" << deduction.second << endl;
+        }
+    }
+
+    double calculateTax(double income, double totalDeductions, int recursiveLevel) const {
+        if (recursiveLevel <= 0) {
+            return 0.0;
+        }
+
+        double taxAmount = 0.15 * income;
+
+        return taxAmount;
     }
 
     double calculateTaxRecursive() const {
-        double totalDeductions = deductions.calculateTotalDeductionsRecursive(deductions.head);
-        return (income - totalDeductions);
+        return calculateTax(userIncome, totalDeductions, 5);
+    }
+
+    // getter and setter methods
+    double getTotalDeductions() const {
+        return totalDeductions;
+    }
+
+    void setTotalDeductions(double totalDeductions) {
+        TaxCalculator::totalDeductions = totalDeductions;
     }
 };
 
 int main() {
-    double userIncome;
-    cout << "Enter your income: $";
+    while (true) {
+    double userIncome = 0;
+    double deductionAmount = 0;
+    string command, deductionName;
+
+    cout << "Welcome to our Tax Calculator!\n";
+    cout << "=========================\n";
+
+    cout << "Enter your income: ";
     cin >> userIncome;
-    cin.ignore();  // Consume the newline character in the input buffer
 
-    TaxCalculator taxCalc(userIncome);
+    TaxCalculator taxCalc(userIncome, 0);
 
-    int deductionCount;
-    cout << "Enter the number of deductions: ";
-    cin >> deductionCount;
-    cin.ignore();  // Consume the newline character in the input buffer
+    while (true) {
+        cout << "\nEnter a deduction (or 'done' to finish): ";
+        cin >> deductionName;
 
-    for (int i = 0; i < deductionCount; ++i) {
-        char deductionName[50];
-        double deductionAmount;
+        if (deductionName == "done") {
+            break;
+        }
 
-        cout << "Enter deduction name: ";
-        cin.getline(deductionName, sizeof(deductionName));
-
-        cout << "Enter deduction amount: $";
+        cout << "\nEnter the amount for the " << deductionName << " deduction: ";
         cin >> deductionAmount;
-        cin.ignore();  // Consume the newline character in the input buffer
 
         taxCalc.addDeduction(deductionName, deductionAmount);
+        taxCalc.sortDeductions();
+        taxCalc.printDeductions();
     }
 
-    double taxAmount = taxCalc.calculateTax();
-    cout << "Tax amount: $" << taxAmount << endl;
+    cout << setw(20) << left << "Total Deductions:" << setw(10) << right << "$" << taxCalc.getTotalDeductions() << endl;
 
-    double taxAmountRecursive = taxCalc.calculateTaxRecursive();
-    cout << "Tax amount (recursive): $" << taxAmountRecursive << endl;
+    double taxAmount = taxCalc.calculateTaxRecursive();
 
-    return 0;
+    cout << setw(20) << left << "Net Income:" << setw(10) << right << "$" << taxAmount << endl;
+
+    // Ask the user if they want to calculate another tax
+    cout << "\nWould you like to calculate another tax? (y/n): ";
+    char choice;
+    cin >> choice;
+
+    if (choice != 'y' && choice != 'Y') {
+        break;
+    }
 }
+
+return 0;
+}
+
